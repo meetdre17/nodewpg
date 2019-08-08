ddb4b157ebd937d858705d863d6aaeef17e00ddf
Samuel Enikanolaye <senikanolaye@gmail.com>
2019-08-08 18:13:55 -0500

    Added new JS exercises

---
+++ javascript/index41.js
@@ -0,0 +1,5 @@
+const text1 = 'HELLO ';
+const text2 = 'javascript ';
+const text3 = 'WORLD ';
+
+console.log((text1.toLowerCase()).concat(text2.toUpperCase()).concat(text3.toLowerCase()));
---
+++ javascript/index43.js
@@ -0,0 +1,9 @@
+const text = 'game of thrones';
+const firstChar = text.charAt(0);
+const secondChar = text.charAt(5);
+const thirdChar = text.charAt(8);
+// const thirdChar = text.charAt(1000);
+
+const result = firstChar + secondChar + thirdChar;
+
+console.log(`The final result is`, result.toUpperCase());
