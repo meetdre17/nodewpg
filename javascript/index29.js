c22f503cf153e71ae5b37febe63221b00c6e333a
Samuel Enikanolaye <senikanolaye@gmail.com>
2019-08-08 18:20:46 -0500

    Edited index29 slightly

--- javascript/index29.js
+++ javascript/index29.js
@@ -1,4 +1,4 @@
 const sideLength = 10;
-const result = sideLength * 4;
+const perimeter = sideLength * 4;
 
-console.log(`To calculate the perimeter of a square, we multiply the length of one side by four. For this square, the length is 10 so therefore, the perimeter is ${sideLength} * ${4} which is equal to`, result);
+console.log(`To calculate the perimeter of a square, we multiply the length of one side by four. For this square, the length is 10 so therefore, the perimeter is ${sideLength} * ${4} which is equal to`, perimeter);
